c5a622a689cb72edb10aa08a7436f63cd7490cd3
NIST\crosin <caroline.rosin@nist.gov>
2017-07-07 15:43:48 -0400

    upgrade to validation engine version

--- src/test/java/gov/nist/healthcare/mu/loi/TestPackage.java
+++ src/test/java/gov/nist/healthcare/mu/loi/TestPackage.java
@@ -8,10 +8,10 @@
  */
 package gov.nist.healthcare.mu.loi;
 
+import gov.nist.healthcare.mu.bundle.documentation.model.json.JTestObject;
 import gov.nist.healthcare.mu.bundle.documentation.model.json.JTestStep;
 import gov.nist.validation.report.Entry;
 import gov.nist.validation.report.Report;
-import hl7.v2.profile.Profile;
 import hl7.v2.profile.XMLDeserializer;
 import hl7.v2.validation.SyncHL7Validator;
 import hl7.v2.validation.content.ConformanceContext;
@@ -29,35 +29,65 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.IOFileFilter;
 import org.apache.commons.io.filefilter.SuffixFileFilter;
 import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
  * @author Caroline Rosin (NIST)
  */
 // @Ignore
-public class TestPackageNEW {
+public class TestPackage {
 
-    private static Logger logger = Logger.getLogger(TestPackageNEW.class.getName());
+    private static Logger logger = Logger.getLogger(TestPackage.class.getName());
 
     private static LOIContext loiContext;
+    private static String globalFolder = "/Global";
+
+    private static String profiles;
+    private static String constraints;
+    private static String valueSets;
 
     @BeforeClass
     public static void setUp() {
         loiContext = new LOIContext();
+        profiles = StringUtils.join(globalFolder, "/Profiles",
+                "/LOI_integration_profile.xml");
+        constraints = StringUtils.join(globalFolder, "/Constraints",
+                "/LOI_Constraints.xml");
+        valueSets = StringUtils.join(globalFolder, "/Tables",
+                "/LOI_ValueSet_Library.xml");
 
     }
 
     @Test
-    public void testValidatePackage() throws Exception {
-        // File f = new File(loiContext.getTESTCASE_DIR_F());
+    public void testValidateTestMessage() throws Exception {
+        System.out.println("*** Validate test messages ***");
         File f = new File("./src/test/resources/messages");
+        validateRepository(f);
+    }
+
+    @Test
+    @Ignore
+    public void testValidateContextFree() throws Exception {
+        System.out.println("*** Validate context-free messages ***");
+        File f = new File(loiContext.getCONTEXT_FREE_DIR_F());
+        validateRepository(f);
+    }
+
+    @Test
+    @Ignore
+    public void testValidateContextBased() throws Exception {
+        System.out.println("*** Validate context-based messages ***");
+        File f = new File(loiContext.getTESTCASE_DIR_F());
+        validateRepository(f);
+    }
+
+    private static void validateRepository(File f) throws Exception {
 
         IOFileFilter msgFilter = new SuffixFileFilter("Message.txt");
-        IOFileFilter conformanceContextFilter = new SuffixFileFilter(
-                "Constraints.xml");
-
         Collection<File> messages = FileUtils.listFiles(f, msgFilter,
                 TrueFileFilter.INSTANCE);
 
@@ -66,10 +96,7 @@ public class TestPackageNEW {
             String messageString = FileUtils.readFileToString(message);
 
             // get conformance context
-            Collection<File> confCtxts = FileUtils.listFiles(
-                    message.getParentFile(), conformanceContextFilter,
-                    TrueFileFilter.INSTANCE);
-
+            File confCtxt = new File(message.getParentFile(), "Constraints.xml");
             File json = new File(message.getParentFile(), "TestStep.json");
             if (!json.exists()) {
                 json = new File(message.getParentFile(), "TestObject.json");
@@ -78,62 +105,33 @@ public class TestPackageNEW {
             String jsonSt = FileUtils.readFileToString(json);
             JTestStep js = JTestStep.fromJson(jsonSt);
             String profile = js.getMessageId();
+            if (profile == null) {
+                JTestObject jo = JTestObject.fromJson(jsonSt);
+                profile = jo.getMessageId();
+            }
             if (profile != null) {
-                if (confCtxts.size() == 0) {
-                    SyncHL7Validator validator = createValidator(
-                            "/Global/Profiles/LOI_integration_profile.xml",
-                            "/Global/Tables/LOI_ValueSet_Library.xml");
+                // create validator
+                SyncHL7Validator validator = createValidator(profiles,
+                        constraints, confCtxt, valueSets);
 
-                    System.out.println("Validating " + folderName);
-                    Report report = validator.check(messageString, profile);
-                    Set<String> keys = report.getEntries().keySet();
-                    for (String key : keys) {
-                        List<Entry> entries = report.getEntries().get(key);
-                        if (entries != null && entries.size() > 0) {
-                            System.out.println("*** " + key + " ***");
-                            for (Entry entry : entries) {
-//                                printEntry(entry);
-                                 if
-                                 (entry.getClassification().equals("Error")) {
-                                 printEntry(entry);
-                                 }
-                                 if
-                                 (entry.getClassification().equals("Alert")) {
-                                 printEntry(entry);
-                                 }
-                            }
-                        }
-                    }
+                System.out.println("Validating " + folderName);
+                System.out.println(messageString);
 
-                }
-                for (File confCtxt : confCtxts) {
-                    FileInputStream fis = new FileInputStream(confCtxt);
-                    SyncHL7Validator validator = createValidator(
-                            "/Global/Profiles/LOI_integration_profile.xml",
-                            fis, "/Global/Tables/LOI_ValueSet_Library.xml");
+                Report report = validator.check(messageString, profile);
 
-                    System.out.println("Validating " + folderName);
-                    Report report = validator.check(messageString, profile);
-                    Set<String> keys = report.getEntries().keySet();
-                    for (String key : keys) {
-                        List<Entry> entries = report.getEntries().get(key);
-                        if (entries != null && entries.size() > 0) {
-                            System.out.println("*** " + key + " ***");
-                            for (Entry entry : entries) {
+                Set<String> keys = report.getEntries().keySet();
+                for (String key : keys) {
+                    List<Entry> entries = report.getEntries().get(key);
+                    if (entries != null && entries.size() > 0) {
+                        System.out.println("*** " + key + " ***");
+                        for (Entry entry : entries) {
+                            switch (entry.getClassification()) {
+                            case "Error":
+                            case "Alert":
                                 printEntry(entry);
-                                // if
-                                // (entry.getClassification().equals("Error")) {
-                                // printEntry(entry);
-                                //
-                                // }
-                                // if
-                                // (entry.getClassification().equals("Alert")) {
-                                // printEntry(entry);
-                                // }
                             }
                         }
                     }
-                    // System.out.println(report.toText());
                 }
             }
         }
@@ -147,63 +145,43 @@ public class TestPackageNEW {
         }
     }
 
-    private static SyncHL7Validator createValidator(String profileFileName,
-            InputStream constraints, String valueSetLibFileName)
+    private static SyncHL7Validator createValidator(
+            String globalProfileFileName, String globalConstraintsFileName,
+            File specificConstraintsFile, String globalValueSetLibFileName)
             throws Exception {
 
         // The profile in XML
-        InputStream profileXML = TestPackageNEW.class.getResourceAsStream(profileFileName);
+        InputStream profileXML = TestPackage.class.getResourceAsStream(globalProfileFileName);
 
-        // ### [Update]
-        // The conformance context file XML
+        // The default conformance context file XML
+        InputStream contextXML = TestPackage.class.getResourceAsStream(globalConstraintsFileName);
 
-        // default constraint file
-        InputStream contextXML2 = TestPackageNEW.class.getResourceAsStream(loiContext.getCONFORMANCE_CONTEXT());
-        List<InputStream> confContexts = Arrays.asList(constraints, contextXML2);
-        // List<InputStream> confContexts = Arrays.asList(constraints);
+        // The test case specific conformance context file XML
+        List<InputStream> confContexts = null;
+        if (specificConstraintsFile != null && specificConstraintsFile.exists()) {
+            InputStream specificContextXML = new FileInputStream(
+                    specificConstraintsFile);
+            confContexts = Arrays.asList(contextXML, specificContextXML);
+        } else {
 
-        ConformanceContext context = DefaultConformanceContext.apply(
-                confContexts).get();
-
-        // The get() at the end will throw an exception if something goes wrong
-        Profile profile = XMLDeserializer.deserialize(profileXML).get();
-
-        InputStream vsLibXML = TestPackageNEW.class.getResourceAsStream(valueSetLibFileName);
-        ValueSetLibrary valueSetLibrary = ValueSetLibraryImpl.apply(vsLibXML).get();
-
-        // A java friendly version of an HL7 validator
-        // This should be a singleton for a specific tool. We create it once and
-        // reuse it across validations
-        return new SyncHL7Validator(profile, valueSetLibrary, context);
-    }
-
-    private static SyncHL7Validator createValidator(String profileFileName,
-            String valueSetLibFileName) throws Exception {
-
-        // The profile in XML
-        InputStream profileXML = TestPackageNEW.class.getResourceAsStream(profileFileName);
-
-        // ### [Update]
-        // The conformance context file XML
-
-        // default constraint file
-        InputStream contextXML2 = TestPackageNEW.class.getResourceAsStream(loiContext.getCONFORMANCE_CONTEXT());
-        List<InputStream> confContexts = Arrays.asList(contextXML2);
-        // List<InputStream> confContexts = Arrays.asList(constraints);
+            confContexts = Arrays.asList(contextXML);
+        }
 
         ConformanceContext context = DefaultConformanceContext.apply(
                 confContexts).get();
 
+        // The value set library file XML
+        InputStream vsLibXML = TestPackage.class.getResourceAsStream(globalValueSetLibFileName);
+
         // The get() at the end will throw an exception if something goes wrong
-        Profile profile = XMLDeserializer.deserialize(profileXML).get();
 
-        InputStream vsLibXML = TestPackageNEW.class.getResourceAsStream(valueSetLibFileName);
+        hl7.v2.profile.Profile profile = XMLDeserializer.deserialize(profileXML).get();
         ValueSetLibrary valueSetLibrary = ValueSetLibraryImpl.apply(vsLibXML).get();
-
         // A java friendly version of an HL7 validator
-        // This should be a singleton for a specific tool. We create it once and
+        // This should be a singleton for a specific tool. We create it once
+        // and
         // reuse it across validations
         return new SyncHL7Validator(profile, valueSetLibrary, context);
-    }
 
+    }
 }
